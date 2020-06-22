87d01241b9e677c651ac79130d5e074aee753a0a
Skullyfox <Skullyfox.yt@gmail.com>
2020-06-22 09:26:03 +0200

    Test command removed

--- events/eventHandler.js
+++ events/eventHandler.js
@@ -3,9 +3,6 @@ const guildMemberAdd = require("../command/guildMemberAdd");
 module.exports = async (Discord, client) => {
     client.on('message', (msg) => {
         const command = msg.content.split(' ')[0];
-        if (command === 'f.test') {
-            client.emit("guildMemberAdd", msg.member || msg.guild.fetchMember(msg.author));
-        }
     });
 
     client.on('guildMemberAdd', (member) => {
